949d573abf8dfa2e167616561b4fb8523873cda8
barko <barkhitordtroll@gmail.com>
2023-07-30 15:27:59 +0800

    version con estilos de login

---
+++ public/login.js
@@ -0,0 +1,21 @@
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+    const screenWidth = window.innerWidth;
+    console.log(screenWidth)
+    if (screenWidth >= 750) {
+        const h1 = document.getElementById("h1");
+      h1.innerHTML = "Iniciar Sesion"
+
+    }
+})
+
+window.addEventListener('resize',()=>{
+    const screenWidth = window.innerWidth;
+    console.log(screenWidth)
+    if (screenWidth >= 750) {
+        const h1 = document.getElementById("h1");
+      h1.innerHTML = "Iniciar Sesion"
+
+    }
+})
